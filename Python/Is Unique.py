f78253ee6ee441f9d6ef5d0ad9edb24604a51ca7
anhtuan <anhtuan.pham@ucalgary.ca>
2021-04-18 20:21:52 -0600

    improve

--- Python/Is Unique.py
+++ Python/Is Unique.py
@@ -3,17 +3,29 @@
 #1.1 CTCI
 #runtime O(n^2)
 
+# def isUnique (s:str):
+#     char = ''
+#     for i in s:
+#         char = i
+#         s = s[1:]
+#         for j in s:
+#             if i == j:
+#                 print(False)
+#                 return False
+#     print(True)
+#     return True
+
+
 def isUnique (s:str):
-    char = ''
-    for i in s:
-        char = i
-        s = s[1:]
-        for j in s:
-            if i == j:
-                print(False)
-                return False
+    temp = ''.join(sorted(s))
+
+    for i in range (0, len(temp) - 1):
+        if temp[i] == temp[i+1]:
+            print(False)
+            return False
     print(True)
     return True
+
 
 isUnique('aaaa') #False
 isUnique('abcd') #True
